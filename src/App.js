67c38ae1eb7e9e5423ddbe0ddbea0b5955f5a356
JOSY12 <josmer1997@hotmail.es>
2022-11-17 12:05:22 -0500

    frontonlyread

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import "./App.css";
-
+var incremental = 0;
 function App() {
   const [employees, setemployees] = useState([
     {
@@ -11,6 +12,7 @@ function App() {
     },
   ]);
   const [employe, setemploye] = useState({
+    id: 0,
     Name: "",
     Lastname: "",
     Salary: 1000,
@@ -18,21 +20,26 @@ function App() {
 
   function addemployee(e) {
     e.preventDefault();
-    employees.push(employe);
+    setemployees([...employees, employe]);
+    incremental++;
+    setemploye({ ...employe, id: incremental });
   }
 
   function setchanges(e) {
     const property = e.target.name;
     const value = e.target.value;
+
     setemploye({ ...employe, [property]: value });
   }
 
-  function deleteworker(Name) {
-    const filtered = employees.filter((e) => e.Name !== Name);
+  function deleteworker(id) {
+    const filtered = employees.filter((e) => e.id !== id);
 
     setemployees(filtered);
   }
+  console.log(employees);
   useEffect(() => {}, [employees, employe]);
+
   return (
     <div className="App-header ">
       <div className=" ">
@@ -83,32 +90,48 @@ function App() {
             {employees.map((e, i) => {
               return (
                 <ul className="row text  container m-0 p-0 ">
-                  <li key={e.Name} className=" col   text-start text-break">
+                  <li key={e.Name + i} className=" col   text-start text-break">
                     {e.Name}
                   </li>
-                  <li key={e.Lastname} className=" col  text-start text-break">
+                  <li
+                    key={e.Lastname + i + 1}
+                    className=" col  text-start text-break"
+                  >
                     {e.Lastname}
                   </li>
-                  <li key={e.Salary} className=" col   text-start text-break">
+                  <li
+                    key={e.Salary + i}
+                    className=" col   text-start text-break"
+                  >
                     {e.Salary}$
                   </li>
 
-                  <div className="col  ">
+                  <div key={e.Salary + i + 2} className="col  ">
                     <ul
+                      key={e.Salary + i + 12}
                       className="row  
                      m-0 p-0   text"
                     >
-                      <li className="row col-sm-6 col-md-6  col-lg-6 m-0 p-0  ">
-                        <button className=" btn btn-info">Edit</button>
+                      <li
+                        key={i + 1}
+                        className="row col-sm-6 col-md-6  col-lg-6 m-0 p-0  "
+                      >
+                        <button key={i + 12} className=" btn btn-info">
+                          Edit
+                        </button>
                       </li>
-                      <li className="row  col-sm-6 col-md-6 col-lg-6 m-0 p-0  ">
+                      <li
+                        key={i + 3}
+                        className="row  col-sm-6 col-md-6 col-lg-6 m-0 p-0  "
+                      >
                         <button
+                          key={i + 13}
                           onClick={() => {
-                            deleteworker(e.Name);
+                            deleteworker(e.id);
                           }}
                           className=" btn btn-danger"
                         >
-                          delete
+                          X
                         </button>
                       </li>
                     </ul>
